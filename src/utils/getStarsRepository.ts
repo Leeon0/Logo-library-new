603a17f5a93a408c08c70daca2be885c89366224
Leeon0 <leoroquett16@gmail.com>
2024-03-15 12:55:26 +0000

    second commit

--- src/utils/getStarsRepository.ts
+++ src/utils/getStarsRepository.ts
@@ -1,5 +1,5 @@
 export async function fetchGitHubStars() {
-  const res = await fetch('https://api.github.com/repos/pheralb/svgl');
+  const res = await fetch('https://api.github.com/repos/Leeon0/Logo-library');
   const response = await res.json();
   const starsFormated =
     response.stargazers_count > 1000
